25fe45ac91dc3d4ef32cd1c0324a5017957351b4
Антон Кутовой <antonykutovoi@gmail.com>
2020-12-15 14:51:13 +0300

    Сделал вывод коллекции продуктов (видеокарты)

--- Shop/Shop/Controllers/HomeController.cs
+++ Shop/Shop/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,18 @@ namespace Shop.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index(int id)
+        //public IActionResult Index(int id)
+        //{
+        //    return View();
+        //}
+        MobileContext db;
+        public HomeController(MobileContext context)
         {
-            return View();
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
         }
     }
 }
