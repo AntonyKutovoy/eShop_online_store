f7e506992ec6b2ccb3105d494f40afba7efcb6ae
Антон Кутовой <antonykutovoi@gmail.com>
2020-12-15 13:07:34 +0300

    Изменил класс Program, чтобы инициализатор базы данных вызывался при старте приложения

--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Shop.Models;
+using System;
 
 namespace Shop
 {
@@ -7,8 +11,26 @@ namespace Shop
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<MobileContext>();
+                    SampleData.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
+
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
