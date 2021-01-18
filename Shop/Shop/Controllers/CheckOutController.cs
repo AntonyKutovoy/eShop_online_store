462a46f0601ee4626e1af666cbd5ff004b143a01
Антон Кутовой <antonykutovoi@gmail.com>
2021-01-18 17:38:57 +0300

    Сделал страницу чекаута, добавил поиск и иконку для логина

---
+++ Shop/Shop/Controllers/CheckOutController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        private readonly CartService cartService;
+        private readonly ProductService productService;
+        private Guid userId = new Guid("0f8fad5b-d9cb-469f-a165-70867728950e");//временная переменная для проверок
+        public CheckOutController(CartService cartService, ProductService productService)
+        {
+            this.cartService = cartService;
+            this.productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["productInCartCount"] = cartService.GetCurrentCart(userId).AllAmount;
+            return View();
+        }
+    }
+}
