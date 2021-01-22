d5a9756f04f6681680449eeb1c6cfc5280f38ef6
Josef Dzeranov <soslan29@gmail.com>
2021-01-22 20:11:48 +0300

    Сделали поиск и пагинацию над ней

--- Shop/Shop/Controllers/HomeController.cs
+++ Shop/Shop/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Shop.DataAccess;
 using Shop.Models;
 using Shop.Services;
 using System;
@@ -14,7 +13,6 @@ namespace Shop.Controllers
         private readonly ProductService productService;
         private readonly CartService cartService;
         private Guid userId = new Guid("0f8fad5b-d9cb-469f-a165-70867728950e");//временная переменная для проверок
-
         public HomeController(ProductService productService, CartService cartService)
         {
             this.productService = productService;
@@ -23,34 +21,41 @@ namespace Shop.Controllers
         public IActionResult Index(int page = 1)
         {
             var products = productService.GetAllProducts();
-            var productsOnCurrentPage = products.Skip((page - 1) * productsCountPerPage).Take(productsCountPerPage).ToList();
+            var productsOnCurrentPage = CreatePagination(products, page);
+            CreateCartProductsCount();
+            ViewData["methodForPagination"] = "Index";
+            return View(productsOnCurrentPage);
+        }
 
+        private void CreateCartProductsCount()
+        {
+            ViewData["cartProductsCount"] = cartService.GetCurrentCart(userId).AllAmount;
+        }
+
+        private List<ProductViewModel> CreatePagination(List<ProductViewModel> products, int page)
+        {
+            var productsOnCurrentPage = products.Skip((page - 1) * productsCountPerPage).Take(productsCountPerPage).ToList();
             var countPages = products.Count / productsCountPerPage;
             if (products.Count % productsCountPerPage > 0)
             {
                 countPages++;
             }
             ViewData["countPages"] = countPages;
-            ViewData["cartProductsCount"] = cartService.GetCurrentCart(userId).AllAmount;
-            return View(productsOnCurrentPage);
+            return productsOnCurrentPage;
         }
 
-        public IActionResult Search(string searchproduct)
+        public IActionResult Search(string id, int page = 1)
         {
             var products = productService.GetAllProducts();
             var searchProducts = new List<ProductViewModel>();
-            if (!String.IsNullOrEmpty(searchproduct))
+            if (!string.IsNullOrEmpty(id))
             {
-                foreach (var searchProduct in products)
-                {
-                    if (searchProduct.Name.Contains(searchproduct))
-                    {
-                        searchProducts.Add(searchProduct);
-                        ViewData["test"] = searchProducts.Count();//переменная для проверки (удалить)
-                    }
-                }
+                searchProducts = products.Where(x => x.Name.Contains(id)).ToList();
             }
-            return RedirectToAction("Index");
+            var productsOnCurrentPage = CreatePagination(searchProducts, page);
+            CreateCartProductsCount();
+            ViewData["methodForPagination"] = "Search/" + id;
+            return View("Index", productsOnCurrentPage);
         }
     }
 }
