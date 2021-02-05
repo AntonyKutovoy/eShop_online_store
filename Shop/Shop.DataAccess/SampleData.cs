a20e9cdb4adeef6824476c5dc3f78f27fdeaaf47
Josef Dzeranov <soslan29@gmail.com>
2021-02-05 20:57:54 +0300

    Убрали ID

--- Shop/Shop.DataAccess/SampleData.cs
+++ Shop/Shop.DataAccess/SampleData.cs
@@ -11,35 +11,30 @@ namespace Shop.DataAccess
             var products = new List<Product> {
                     new Product
                     {
-                        Id = Guid.NewGuid(),
                         Name = "NVIDIA GeForce RTX 3070",
                         Price = 70000,
                         Description = "Здесь будет описание RTX 3070."
                     },
                     new Product
                     {
-                        Id = Guid.NewGuid(),
                         Name = "NVIDIA GeForce RTX 3080",
                         Price = 100000,
                         Description = "Здесь будет описание RTX 3080."
                     },
                     new Product
                     {
-                        Id = Guid.NewGuid(),
                         Name = "NVIDIA GeForce RTX 3090",
                         Price = 155000,
                         Description = "Здесь будет описание RTX 3090."
                     },
                     new Product
                     {
-                        Id = Guid.NewGuid(),
                         Name = "NVIDIA GeForce RTX 3060TI",
                         Price = 55000,
                         Description = "Здесь будет описание RTX 3060TI."
                     },
                     new Product
                     {
-                        Id = Guid.NewGuid(),
                         Name = "NVIDIA GeForce RTX 3050",
                         Price = 45000,
                         Description = "Здесь будет описание RTX 3050."
@@ -50,7 +45,6 @@ namespace Shop.DataAccess
             {
                 products.Add(new Product
                 {
-                    Id = Guid.NewGuid(),
                     Name = "NVIDIA GeForce " + i,
                     Price = i * 10000,
                     Description = "Здесь будет описание RTX." + i
