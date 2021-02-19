b51ff348a8481e3a1eb2263de0e1dfba39ef9fe3
Антон Кутовой <antonykutovoi@gmail.com>
2021-02-19 18:39:57 +0300

    добавил редактирование/добавление ролей и прав для пользователей;

---
+++ Shop/Shop/Controllers/RolesController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Shop.DataAccess;
+using Shop.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CustomIdentityApp.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RolesController : Controller
+    {
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<ApplicationUser> _userManager;
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index() => View(_roleManager.Roles.ToList());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                var result = _roleManager.CreateAsync(new IdentityRole(name)).Result;
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            var role = _roleManager.FindByIdAsync(id).Result;
+            if (role != null)
+            {
+                var result = _roleManager.DeleteAsync(role).Result;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UserList() => View(_userManager.Users.ToList());
+
+        public IActionResult Edit(string userId)
+        {
+            // получаем пользователя
+            var user = _userManager.FindByIdAsync(userId).Result;
+            if (user != null)
+            {
+                // получем список ролей пользователя
+                var userRoles = _userManager.GetRolesAsync(user).Result;
+                var allRoles = _roleManager.Roles.ToList();
+                var model = new ChangeRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserEmail = user.Email,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+
+            return NotFound();
+        }
+        [HttpPost]
+        public IActionResult Edit(string userId, List<string> roles)
+        {
+            // получаем пользователя
+            var user = _userManager.FindByIdAsync(userId).Result;
+            if (user != null)
+            {
+                // получем список ролей пользователя
+                var userRoles = _userManager.GetRolesAsync(user).Result;
+                // получаем все роли
+                var allRoles = _roleManager.Roles.ToList();
+                // получаем список ролей, которые были добавлены
+                var addedRoles = roles.Except(userRoles);
+                // получаем роли, которые были удалены
+                var removedRoles = userRoles.Except(roles);
+
+                _userManager.AddToRolesAsync(user, addedRoles);
+
+                _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+                return RedirectToAction("UserList");
+            }
+
+            return NotFound();
+        }
+    }
+}
